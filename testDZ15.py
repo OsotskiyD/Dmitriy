c14d48d3d1b57b16239a8cf7e0566599a5b5ff71
Osotskiy Dmitriy <56317491+OsotskiyD@users.noreply.github.com>
2019-12-07 16:43:51 +0200

    Delete testDZ15.py

--- testDZ15.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt
-def geo(a, b):
-    a=float(input('Введите первый катет: '))
-    b=float(input('Введите гипотенузу: '))
-    k=sqrt(b*b-a*a)
-    r=a*k/(a+b+k)
-    print( "второй катет: ", k, "радиус вписаной окружности: ", r)
